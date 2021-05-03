cfbc2e70ffd3a524e4c82283765361436be59c3c
PlusLius <408709581@qq.com>
2021-05-04 01:24:36 +0800

    Update index.js

--- src/core/index.js
+++ src/core/index.js
@@ -1,10 +1,11 @@
 //Vue 实例和Vue API
 import Vue from './instance/index'
-import { initGlobalAPI } from './global-api/index'
+import { initGlobalAPI } from './global-api/index'https://github.com/PlusLius/vue/blob/dev/src/core/index.js
 import { isServerRendering } from 'core/util/env'
 import { FunctionalRenderContext } from 'core/vdom/create-functional-component'
 
 //将Vue作为构造函数作为参数，传递给initGlobalAPI
+// 见名知意，Vue的全局方法全部初始化了还提供了一些工具方法
 initGlobalAPI(Vue)
 
 //挂载isServer
