be01c1ed13d04e681d16095bf674cca7814f9eb0
PlusLius <408709581@qq.com>
2021-05-05 07:43:35 +0800

    Update resolve-async-component.js

--- src/core/vdom/helpers/resolve-async-component.js
+++ src/core/vdom/helpers/resolve-async-component.js
@@ -23,7 +23,7 @@ function ensureCtor (comp: any, base) {
     ? base.extend(comp)
     : comp
 }
-
+// 创建异步组件占位符
 export function createAsyncPlaceholder (
   factory: Function,
   data: ?VNodeData,
@@ -37,6 +37,36 @@ export function createAsyncPlaceholder (
   return node
 }
 
+// 解析异步组件
+// 1. Promise异步组件
+// Vue.component(
+//   'async-webpack-example',
+//   // 该 `import` 函数返回一个 `Promise` 对象。
+//   () => import('./my-async-component')
+// )
+
+// 2. 高级异步组件
+// const AsyncComp = () => ({
+//   // 需要加载的组件。应当是一个 Promise
+//   component: import('./MyComp.vue'),
+//   // 加载中应当渲染的组件
+//   loading: LoadingComp,
+//   // 出错时渲染的组件
+//   error: ErrorComp,
+//   // 渲染加载中组件前的等待时间。默认：200ms。
+//   delay: 200,
+//   // 最长等待时间。超出此时间则渲染错误组件。默认：Infinity
+//   timeout: 3000
+// })
+// Vue.component('async-example', AsyncComp)
+
+// 3. 普通异步组件
+// Vue.component('async-example', function (resolve, reject) {
+//    // 这个特殊的 require 语法告诉 webpack
+//    // 自动将编译后的代码分割成不同的块，
+//    // 这些块将通过 Ajax 请求自动下载。
+//    require(['./my-async-component'], resolve)
+// })
 export function resolveAsyncComponent (
   factory: Function,
   baseCtor: Class<Component>,
