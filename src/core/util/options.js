27e83966c1f002a814a37cb4a9c8c4508c336331
PlusLius <408709581@qq.com>
2021-05-04 11:54:53 +0800

    Update options.js

--- src/core/util/options.js
+++ src/core/util/options.js
@@ -413,42 +413,42 @@ function assertObjectType (name: string, value: any, vm: ?Component) {
  
  1. new Vue的时候调用
  2. Vue.extend的时候调用
- 
- vm.$options = {
-  parent: Vue /*父Vue实例*/,
-  propsData: undefined,
-  _componentTag: undefined,
-  _parentVnode: VNode /*父VNode实例*/,
-  _renderChildren:undefined,
-  __proto__: {
-    components: { },
-    directives: { },
-    filters: { },
-    _base: function Vue(options) {
-        //...
-    },
-    _Ctor: {},
-    created: [
-      function created() {
-        console.log('parent created')
-      }, function created() {
-        console.log('child created')
-      }
-    ],
-    mounted: [
-      function mounted() {
-        console.log('child mounted')
-      }
-    ],
-    data() {
-       return {
-         msg: 'Hello Vue'
-       }
-    },
-    template: '<div>{{msg}}</div>'
-  }
-}
- */
+ // vm.$options = {
+//     parent: Vue /*父Vue实例*/,
+//     propsData: undefined,
+//     _componentTag: undefined,
+//     _parentVnode: VNode /*父VNode实例*/,
+//     _renderChildren:undefined,
+//     __proto__: {
+//       components: { },
+//       directives: { },
+//       filters: { },
+//       _base: function Vue(options) {
+//           //...
+//       },
+//       _Ctor: {},
+//       created: [
+//         function created() {
+//           console.log('parent created')
+//         }, function created() {
+//           console.log('child created')
+//         }
+//       ],
+//       mounted: [
+//         function mounted() {
+//           console.log('child mounted')
+//         }
+//       ],
+//       data() {
+//          return {
+//            msg: 'Hello Vue'
+//          }
+//       },
+//       template: '<div>{{msg}}</div>'
+//     }
+//   }
+
+**/
 export function mergeOptions (
   parent: Object,
   child: Object,
