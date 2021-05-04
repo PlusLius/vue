58ded3731d1025d9921e2501107316364a757d44
PlusLius <408709581@qq.com>
2021-05-04 11:52:43 +0800

    Update options.js

--- src/core/util/options.js
+++ src/core/util/options.js
@@ -410,6 +410,44 @@ function assertObjectType (name: string, value: any, vm: ?Component) {
 /**
  * Merge two option objects into a new one.
  * Core utility used in both instantiation and inheritance.
+ 
+ 1. new Vue的时候调用
+ 2. Vue.extend的时候调用
+ 
+ vm.$options = {
+  parent: Vue /*父Vue实例*/,
+  propsData: undefined,
+  _componentTag: undefined,
+  _parentVnode: VNode /*父VNode实例*/,
+  _renderChildren:undefined,
+  __proto__: {
+    components: { },
+    directives: { },
+    filters: { },
+    _base: function Vue(options) {
+        //...
+    },
+    _Ctor: {},
+    created: [
+      function created() {
+        console.log('parent created')
+      }, function created() {
+        console.log('child created')
+      }
+    ],
+    mounted: [
+      function mounted() {
+        console.log('child mounted')
+      }
+    ],
+    data() {
+       return {
+         msg: 'Hello Vue'
+       }
+    },
+    template: '<div>{{msg}}</div>'
+  }
+}
  */
 export function mergeOptions (
   parent: Object,
