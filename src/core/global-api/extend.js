d43c12138658092c4aa90cbe49dfac41c313974f
PlusLius <408709581@qq.com>
2021-05-05 20:00:07 +0800

    Update extend.js

--- src/core/global-api/extend.js
+++ src/core/global-api/extend.js
@@ -16,9 +16,13 @@ export function initExtend (Vue: GlobalAPI) {
   /**
    * Class inheritance
    */
+//   Vue.extend 的作用就是构造一个 Vue 的子类，它使用一种非常经典的原型继承的方式把一个纯对象转换一个继承于 Vue 的构造器 Sub 并返回
+//   后对 Sub 这个对象本身扩展了一些属性，如扩展 options、添加全局 API 等；
+//   并且对配置中的 props 和 computed 做了初始化工作；
+//   最后对于这个 Sub 构造函数做了缓存，避免多次执行 Vue.extend 的时候对同一个子组件重复构造。
   Vue.extend = function (extendOptions: Object): Function {
     extendOptions = extendOptions || {}
-    const Super = this
+    const Super = this // Vue
     const SuperId = Super.cid
     const cachedCtors = extendOptions._Ctor || (extendOptions._Ctor = {})
     if (cachedCtors[SuperId]) {
@@ -29,30 +33,35 @@ export function initExtend (Vue: GlobalAPI) {
     if (process.env.NODE_ENV !== 'production' && name) {
       validateComponentName(name)
     }
-
+// 这样当我们去实例化 Sub 的时候，就会执行 this._init 逻辑再次走到了 Vue 实例的初始化逻辑，
     const Sub = function VueComponent (options) {
+//       这样当我们去实例化 Sub 的时候，就会执行 this._init 逻辑再次走到了 Vue 实例的初始化逻
       this._init(options)
     }
+// 它使用一种非常经典的原型继承的方式把一个纯对象转换一个继承于 Vue 的构造器 Sub 并返回
     Sub.prototype = Object.create(Super.prototype)
     Sub.prototype.constructor = Sub
     Sub.cid = cid++
+// 然后对 Sub 这个对象本身扩展了一些属性，如扩展 options 
     Sub.options = mergeOptions(
       Super.options,
       extendOptions
     )
     Sub['super'] = Super
-
     // For props and computed properties, we define the proxy getters on
     // the Vue instances at extension time, on the extended prototype. This
     // avoids Object.defineProperty calls for each instance created.
+// 并且对配置中的 props 
     if (Sub.options.props) {
       initProps(Sub)
     }
+//     和 computed 做了初始化工作；
     if (Sub.options.computed) {
       initComputed(Sub)
     }
 
     // allow further extension/mixin/plugin usage
+//     、添加全局 API 等；
     Sub.extend = Super.extend
     Sub.mixin = Super.mixin
     Sub.use = Super.use
@@ -73,7 +82,7 @@ export function initExtend (Vue: GlobalAPI) {
     Sub.superOptions = Super.options
     Sub.extendOptions = extendOptions
     Sub.sealedOptions = extend({}, Sub.options)
-
+    // 最后对于这个 Sub 构造函数做了缓存，避免多次执行 Vue.extend 的时候对同一个子组件重复构造。
     // cache constructor
     cachedCtors[SuperId] = Sub
     return Sub
