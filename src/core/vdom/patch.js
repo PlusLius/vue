36c7b389cbd41e0e5b9f84d46509d381d8fcd789
PlusLius <408709581@qq.com>
2021-05-05 15:43:40 +0800

    Update patch.js

--- src/core/vdom/patch.js
+++ src/core/vdom/patch.js
@@ -66,12 +66,12 @@ function createKeyToOldIdx (children, beginIdx, endIdx) {
   }
   return map
 }
-
-export function createPatchFunction (backend) {
+// 
+export function createPatchFunction (backend) { // { nodeOps, modules } 里面有平台方法，和平台指令和内置指令
   let i, j
   const cbs = {}
-
-  const { modules, nodeOps } = backend
+// 在这里，nodeOps 表示对 “平台 DOM” 的一些操作方法，modules 表示平台的一些模块，它们会在整个 patch 过程的不同阶段执行相应的钩子函数
+  const { modules, nodeOps } = backend // 里面有平台方法，和平台指令和内置指令
 
   for (i = 0; i < hooks.length; ++i) {
     cbs[hooks[i]] = []
@@ -121,7 +121,7 @@ export function createPatchFunction (backend) {
   }
 
   let creatingElmInVPre = 0
-
+  
   function createElm (
     vnode,
     insertedVnodeQueue,
@@ -141,14 +141,14 @@ export function createPatchFunction (backend) {
     }
 
     vnode.isRootInsert = !nested // for transition enter check
-    if (createComponent(vnode, insertedVnodeQueue, parentElm, refElm)) {
+    if (createComponent(vnode, insertedVnodeQueue, parentElm, refElm)) { // createComponent 方法目的是尝试创建子组件，在当前这个 case 下它的返回值为 false；
       return
     }
 
     const data = vnode.data
     const children = vnode.children
     const tag = vnode.tag
-    if (isDef(tag)) {
+    if (isDef(tag)) { // 接下来判断 vnode 是否包含 tag，如果包含，先简单对 tag 的合法性在非生产环境下做校验，看是否是一个合法标签；
       if (process.env.NODE_ENV !== 'production') {
         if (data && data.pre) {
           creatingElmInVPre++
@@ -162,7 +162,7 @@ export function createPatchFunction (backend) {
           )
         }
       }
-
+      // 然后再去调用平台 DOM 的操作去创建一个占位符元素。
       vnode.elm = vnode.ns
         ? nodeOps.createElementNS(vnode.ns, tag)
         : nodeOps.createElement(tag, vnode)
@@ -188,10 +188,13 @@ export function createPatchFunction (backend) {
           insert(parentElm, vnode.elm, refElm)
         }
       } else {
+        // 接下来调用 createChildren 方法去创建子元素
         createChildren(vnode, children, insertedVnodeQueue)
         if (isDef(data)) {
+          // 接着再调用 invokeCreateHooks 方法执行所有的 create 的钩子并把 vnode push 到 insertedVnodeQueue 中。
           invokeCreateHooks(vnode, insertedVnodeQueue)
         }
+        // 最后调用 insert 方法把 DOM 插入到父节点中，因为是递归调用，子元素会优先调用 insert，所以整个 vnode 树节点的插入顺序是先子后父。
         insert(parentElm, vnode.elm, refElm)
       }
 
@@ -202,11 +205,15 @@ export function createPatchFunction (backend) {
       vnode.elm = nodeOps.createComment(vnode.text)
       insert(parentElm, vnode.elm, refElm)
     } else {
+      // 在 createElm 过程中，如果 vnode 节点不包含 tag，则它有可能是一个注释或者纯文本节点
       vnode.elm = nodeOps.createTextNode(vnode.text)
+      // ，可以直接插入到父元素中。
+      // 最内层就是一个文本 vnode，它的 text 值取的就是之前的 this.message 的值 Hello Vue!。
+      // 往app中插入文本dom
       insert(parentElm, vnode.elm, refElm)
     }
   }
-
+  // createComponent 方法目的是尝试创建子组件，
   function createComponent (vnode, insertedVnodeQueue, parentElm, refElm) {
     let i = vnode.data
     if (isDef(i)) {
@@ -268,11 +275,13 @@ export function createPatchFunction (backend) {
     // a reactivated keep-alive component doesn't insert itself
     insert(parentElm, vnode.elm, refElm)
   }
-
+  // 最后调用 insert 方法把 DOM 插入到父节点中，因为是递归调用，子元素会优先调用 insert，所以整个 vnode 树节点的插入顺序是先子后父。
   function insert (parent, elm, ref) {
     if (isDef(parent)) {
       if (isDef(ref)) {
         if (ref.parentNode === parent) {
+          // insert 逻辑很简单，调用一些 nodeOps 把子节点插入到父节点中
+          // 其实就是调用原生 DOM 的 API 进行 DOM 操作，
           nodeOps.insertBefore(parent, elm, ref)
         }
       } else {
@@ -280,13 +289,15 @@ export function createPatchFunction (backend) {
       }
     }
   }
-
+  // createChildren 方法去创建子元素
   function createChildren (vnode, children, insertedVnodeQueue) {
     if (Array.isArray(children)) {
       if (process.env.NODE_ENV !== 'production') {
         checkDuplicateKeys(children)
       }
+      // createChildren 的逻辑很简单，实际上是遍历子虚拟节点，
       for (let i = 0; i < children.length; ++i) {
+        // 递归调用 createElm, 这是一种常用的深度优先的遍历算法，这里要注意的一点是在遍历过程中会把 vnode.elm 作为父容器的 DOM 节点占位符传入。
         createElm(children[i], insertedVnodeQueue, vnode.elm, null, true, children, i)
       }
     } else if (isPrimitive(vnode.text)) {
@@ -300,14 +311,16 @@ export function createPatchFunction (backend) {
     }
     return isDef(vnode.tag)
   }
-
+  // 接着再调用 invokeCreateHooks 方法执行所有的 create 的钩子并把 vnode push 到 insertedVnodeQueue 中。
   function invokeCreateHooks (vnode, insertedVnodeQueue) {
     for (let i = 0; i < cbs.create.length; ++i) {
+      // 方法执行所有的 create 的钩子
       cbs.create[i](emptyNode, vnode)
     }
     i = vnode.data.hook // Reuse variable
     if (isDef(i)) {
       if (isDef(i.create)) i.create(emptyNode, vnode)
+      // 并把 vnode push 到 insertedVnodeQueue 中。
       if (isDef(i.insert)) insertedVnodeQueue.push(vnode)
     }
   }
@@ -681,9 +694,38 @@ export function createPatchFunction (backend) {
       return node.nodeType === (vnode.isComment ? 8 : 3)
     }
   }
-
-  return function patch (oldVnode, vnode, hydrating, removeOnly) {
-    if (isUndef(vnode)) {
+// createPatchFunction 内部定义了一系列的辅助方法，最终返回了一个 patch 方法，这个方法就赋值给了 vm._update 函数里调用的 vm.__patch__。
+//  patch 是平台相关的，在 Web 和 Weex 环境，它们把虚拟 DOM 映射到 “平台 DOM” 的方法是不同的，
+//  并且对 “DOM” 包括的属性模块创建和更新也不尽相同。因此每个平台都有各自的 nodeOps 和 modules，它们的代码需要托管在 src/platforms 这个大目录下
+  
+//   而不同平台的 patch 的主要逻辑部分是相同的，所以这部分公共的部分托管在 core 这个大目录下。差异化部分只需要通过参数来区别，
+//   这里用到了一个函数柯里化的技巧，通过 createPatchFunction 把差异化参数提前固化，
+//   这样不用每次调用 patch 的时候都传递 nodeOps 和 modules 了，这种编程技巧也非常值得学习。
+  return function patch (oldVnode, vnode, hydrating, removeOnly) { 
+//     patch 方法本身，它接收 4个参数，
+//     oldVnode 表示旧的 VNode 节点，它也可以不存在或者是一个 DOM 对象；
+//     vnode 表示执行 _render 后返回的 VNode 的节点；
+//     hydrating 表示是否是服务端渲染；
+//     removeOnly 是给 transition-group 用的
+    // var app = new Vue({
+    //   el: '#app',
+    //   render: function (createElement) {
+    //     return createElement('div', {
+    //       attrs: {
+    //         id: 'app'
+    //       },
+    //     }, this.message)
+    //   },
+    //   data: {
+    //     message: 'Hello Vue!'
+    //   }
+    // })
+//     vm.$el = vm.__patch__(vm.$el, vnode, hydrating, false /* removeOnly */)
+//     ，我们的场景是首次渲染，所以在执行 patch 函数的时候，传入的 vm.$el 对应的是例子中 id 为 app 的 DOM 对象，
+//     这个也就是我们在 index.html 模板中写的 <div id="app">，
+//     vm.$el 的赋值是在之前 mountComponent 函数做的，vnode 对应的是调用 render 函数的返回值，hydrating 在非服务端渲染情况下为 false，removeOnly 为 false。
+    
+    if (isUndef(vnode)) { 
       if (isDef(oldVnode)) invokeDestroyHook(oldVnode)
       return
     }
@@ -701,7 +743,7 @@ export function createPatchFunction (backend) {
         // patch existing root node
         patchVnode(oldVnode, vnode, insertedVnodeQueue, removeOnly)
       } else {
-        if (isRealElement) {
+        if (isRealElement) { // // 由于我们传入的 oldVnode 实际上是一个 DOM container，所以 isRealElement 为 true
           // mounting to a real element
           // check if this is server-rendered content and if we can perform
           // a successful hydration.
@@ -725,21 +767,23 @@ export function createPatchFunction (backend) {
           }
           // either not server-rendered, or hydration failed.
           // create an empty node and replace it
+          // 接下来又通过 emptyNodeAt 方法把 oldVnode 转换成 VNode 对象，
           oldVnode = emptyNodeAt(oldVnode)
         }
 
         // replacing existing element
-        const oldElm = oldVnode.elm
-        const parentElm = nodeOps.parentNode(oldElm)
+        const oldElm = oldVnode.elm // 实际就是传入的el
+        const parentElm = nodeOps.parentNode(oldElm) // 将el作为父dom
 
         // create new node
-        createElm(
+        // 然后再调用 createElm 方法
+        createElm( // createElm 的作用是通过虚拟节点创建真实的 DOM 并插入到它的父节点中
           vnode,
           insertedVnodeQueue,
           // extremely rare edge case: do not insert if old element is in a
           // leaving transition. Only happens when combining transition +
           // keep-alive + HOCs. (#4590)
-          oldElm._leaveCb ? null : parentElm,
+          oldElm._leaveCb ? null : parentElm, // 传入父dom元素
           nodeOps.nextSibling(oldElm)
         )
 
