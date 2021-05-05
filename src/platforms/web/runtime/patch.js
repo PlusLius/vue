ebc8a5705348bde66bc0d592b400a4077cd02132
PlusLius <408709581@qq.com>
2021-05-05 14:59:48 +0800

    Update patch.js

--- src/platforms/web/runtime/patch.js
+++ src/platforms/web/runtime/patch.js
@@ -8,5 +8,7 @@ import platformModules from 'web/runtime/modules/index'
 // the directive module should be applied last, after all
 // built-in modules have been applied.
 const modules = platformModules.concat(baseModules)
-
+// 该方法的定义是调用 createPatchFunction 方法的返回值，这里传入了一个对象，
+// 包含 nodeOps 参数和 modules 参数。其中，nodeOps 封装了一系列 DOM 操作的方法
+// modules 定义了一些模块的钩子函数的实现
 export const patch: Function = createPatchFunction({ nodeOps, modules })
