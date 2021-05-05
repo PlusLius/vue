8153cba270cb6abc1757975f53ef2096c76442d4
PlusLius <408709581@qq.com>
2021-05-05 14:56:12 +0800

    Update lifecycle.js

--- src/core/instance/lifecycle.js
+++ src/core/instance/lifecycle.js
@@ -48,6 +48,8 @@ export function initLifecycle (vm: Component) {
 }
 
 export function lifecycleMixin (Vue: Class<Component>) {
+// Vue 的 _update 是实例的一个私有方法，它被调用的时机有 2 个，一个是首次渲染，一个是数据更新的时候；
+// _update 方法的作用是把 VNode 渲染成真实的 DOM
   Vue.prototype._update = function (vnode: VNode, hydrating?: boolean) {
     const vm: Component = this
     const prevEl = vm.$el
@@ -57,11 +59,29 @@ export function lifecycleMixin (Vue: Class<Component>) {
     vm._vnode = vnode
     // Vue.prototype.__patch__ is injected in entry points
     // based on the rendering backend used.
+    // _update 的核心就是调用 vm.__patch__ 方法，这个方法实际上在不同的平台，比如 web 和 weex 上的定义是不一样的，
+    // Vue.prototype.__patch__ = inBrowser ? patch : noop
+    // 可以看到，甚至在 web 平台上，是否是服务端渲染也会对这个方法产生影响。因为在服务端渲染中，
+    // 没有真实的浏览器 DOM 环境，所以不需要把 VNode 最终转换成 DOM，因此是一个空函数，而在浏览器端渲染中，它指向了 patch 方法
+//     import * as nodeOps from 'web/runtime/node-ops'
+//     import { createPatchFunction } from 'core/vdom/patch'
+//     import baseModules from 'core/vdom/modules/index'
+//     import platformModules from 'web/runtime/modules/index'
+
+//     // the directive module should be applied last, after all
+//     // built-in modules have been applied.
+//     const modules = platformModules.concat(baseModules)
+ 
+//     export const patch: Function = createPatchFunction({ nodeOps, modules })
+//     该方法的定义是调用 createPatchFunction 方法的返回值，这里传入了一个对象，包含 nodeOps 参数和 modules 参数。
+//     其中，nodeOps 封装了一系列 DOM 操作的方法，modules 定义了一些模块的钩子函数的实现，
     if (!prevVnode) {
       // initial render
+      // 首次渲染
       vm.$el = vm.__patch__(vm.$el, vnode, hydrating, false /* removeOnly */)
     } else {
       // updates
+      // 数据更新时渲染
       vm.$el = vm.__patch__(prevVnode, vnode)
     }
     activeInstance = prevActiveInstance
