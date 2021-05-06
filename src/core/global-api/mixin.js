af173b8d9e5a5821fc947b389f3be515f8d4b2ea
PlusLius <408709581@qq.com>
2021-05-06 13:30:13 +0800

    Update mixin.js

--- src/core/global-api/mixin.js
+++ src/core/global-api/mixin.js
@@ -1,9 +1,23 @@
 /* @flow */
 
 import { mergeOptions } from '../util/index'
+// // 为自定义的选项 'myOption' 注入一个处理器。
+// Vue.mixin({
+//   created: function () {
+//     var myOption = this.$options.myOption
+//     if (myOption) {
+//       console.log(myOption)
+//     }
+//   }
+// })
 
+// new Vue({
+//   myOption: 'hello!'
+// })
+// // => "hello!"
 export function initMixin (Vue: GlobalAPI) {
   Vue.mixin = function (mixin: Object) {
+    // 当前根实例的options, 和需要在全局混入的mixin, 实际修改的是全局的options
     this.options = mergeOptions(this.options, mixin)
     return this
   }
