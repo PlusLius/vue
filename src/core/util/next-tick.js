0f83092af2f37e356cf2c50c78d16369665232f6
PlusLius <408709581@qq.com>
2021-05-07 10:56:25 +0800

    Update next-tick.js

--- src/core/util/next-tick.js
+++ src/core/util/next-tick.js
@@ -1,6 +1,8 @@
 /* @flow */
 /* globals MessageChannel */
-
+// 我们了解到数据的变化到 DOM 的重新渲染是一个异步过程，发生在下一个 tick。这就是我们平时在开发的过程中，
+// 比如从服务端接口去获取数据的时候，数据做了修改，如果我们的某些方法去依赖了数据修改后的 DOM 变化，
+// 我们就必须在 nextTick 后执行。比如下面的伪代码：
 import { noop } from 'shared/util'
 import { handleError } from './error'
 import { isIOS, isNative } from './env'
@@ -8,6 +10,8 @@ import { isIOS, isNative } from './env'
 const callbacks = []
 let pending = false
 
+// 这里使用 callbacks 而不是直接在 nextTick 中执行回调函数的原因是保证在同一个 tick 内多次执行 nextTick，
+// 不会开启多个异步任务，而把这些异步任务都压成一个同步任务，在下一个 tick 执行完毕。
 function flushCallbacks () {
   pending = false
   const copies = callbacks.slice(0)
@@ -34,6 +38,10 @@ let useMacroTask = false
 // in IE. The only polyfill that consistently queues the callback after all DOM
 // events triggered in the same loop is by using MessageChannel.
 /* istanbul ignore if */
+// next-tick.js 申明了 microTimerFunc 和 macroTimerFunc 2 个变量，它们分别对应的是 micro task 的函数和 macro task 的函数。
+// 对于 macro task 的实现，优先检测是否支持原生 setImmediate，这是一个高版本 IE 和 Edge 才支持的特性，
+// 不支持的话再去检测是否支持原生的 MessageChannel，如果也不支持的话就会降级为 setTimeout 0；
+// 而对于 micro task 的实现，则检测浏览器是否原生支持 Promise，不支持的话直接指向 macro task 的实现。
 if (typeof setImmediate !== 'undefined' && isNative(setImmediate)) {
   macroTimerFunc = () => {
     setImmediate(flushCallbacks)
@@ -78,6 +86,8 @@ if (typeof Promise !== 'undefined' && isNative(Promise)) {
  * Wrap a function so that if any code inside triggers state change,
  * the changes are queued using a (macro) task instead of a microtask.
  */
+// withMacroTask 函数，它是对函数做一层包装，确保函数执行过程中对数据任意的修改，
+// 触发变化执行 nextTick 的时候强制走 macroTimerFunc。比如对于一些 DOM 交互事件，如 v-on 绑定的事件回调函数的处理，会强制走 macro task
 export function withMacroTask (fn: Function): Function {
   return fn._withTask || (fn._withTask = function () {
     useMacroTask = true
@@ -86,7 +96,9 @@ export function withMacroTask (fn: Function): Function {
     return res
   })
 }
-
+// nextTick(flushSchedulerQueue) 所用到的函数。它的逻辑也很简单，把传入的回调函数 cb 压入 callbacks 数组，
+// 最后一次性地根据 useMacroTask 条件执行 macroTimerFunc 或者是 microTimerFunc，
+// 而它们都会在下一个 tick 执行 flushCallbacks，flushCallbacks 的逻辑非常简单，对 callbacks 遍历，然后执行相应的回调函数。
 export function nextTick (cb?: Function, ctx?: Object) {
   let _resolve
   callbacks.push(() => {
