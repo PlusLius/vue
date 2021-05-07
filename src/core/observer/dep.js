05b1391a3ac4dfbab430034d7a2191a53de40ca3
PlusLius <408709581@qq.com>
2021-05-07 10:11:28 +0800

    Update dep.js

--- src/core/observer/dep.js
+++ src/core/observer/dep.js
@@ -9,6 +9,10 @@ let uid = 0
  * A dep is an observable that can have multiple
  * directives subscribing to it.
  */
+// Dep 是一个 Class，它定义了一些属性和方法，这里需要特别注意的是它有一个静态属性 target，
+// 这是一个全局唯一 Watcher，这是一个非常巧妙的设计，因为在同一时间只能有一个全局的 Watcher 被计算，
+// 另外它的自身属性 subs 也是 Watcher 的数组。
+// Dep 实际上就是对 Watcher 的一种管理，Dep 脱离 Watcher 单独存在是没有意义的，为了完整地讲清楚依赖收集过程，我们有必要看一下 Watcher 的一些相关实现
 export default class Dep {
   static target: ?Watcher;
   id: number;
