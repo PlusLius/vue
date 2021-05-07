e58929749e8d2b06bca5e59e02b240319a6799ff
PlusLius <408709581@qq.com>
2021-05-07 11:31:47 +0800

    Update traverse.js

--- src/core/observer/traverse.js
+++ src/core/observer/traverse.js
@@ -11,6 +11,14 @@ const seenObjects = new Set()
  * getters, so that every nested property inside the object
  * is collected as a "deep" dependency.
  */
+// traverse 的逻辑也很简单，它实际上就是对一个对象做深层递归遍历，
+// 因为遍历过程中就是对一个子对象的访问，会触发它们的 getter 过程，这样就可以收集到依赖，
+// 也就是订阅它们变化的 watcher，这个函数实现还有一个小的优化，
+// 遍历过程中会把子响应式对象通过它们的 dep id 记录到 seenObjects，避免以后重复访问。
+
+// 那么在执行了 traverse 后，我们再对 watch 的对象内部任何一个值做修改，也会调用 watcher 的回调函数了。
+// 对 deep watcher 的理解非常重要，今后工作中如果大家观测了一个复杂对象，并且会改变对象内部深层某个值的时候也希望触发回调，
+// 一定要设置 deep 为 true，但是因为设置了 deep 后会执行 traverse 函数，会有一定的性能开销，所以一定要根据应用场景权衡是否要开启这个配置。
 export function traverse (val: any) {
   _traverse(val, seenObjects)
   seenObjects.clear()
