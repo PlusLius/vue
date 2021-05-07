5e596a75266bcc600b000f4a17532bcea9643412
PlusLius <408709581@qq.com>
2021-05-07 16:20:25 +0800

    Update mixin.js

--- src/core/global-api/mixin.js
+++ src/core/global-api/mixin.js
@@ -16,6 +16,9 @@ import { mergeOptions } from '../util/index'
 // })
 // // => "hello!"
 export function initMixin (Vue: GlobalAPI) {
+//   它的实现实际上非常简单，就是把要混入的对象通过 mergeOptions 合并到 Vue 的 options 中，
+//   由于每个组件的构造函数都会在 extend 阶段合并 Vue.options 到自身的 options 中，
+//   所以也就相当于每个组件都定义了 mixin 定义的选项。
   Vue.mixin = function (mixin: Object) {
     // 当前根实例的options, 和需要在全局混入的mixin, 实际修改的是全局的options
     this.options = mergeOptions(this.options, mixin)
